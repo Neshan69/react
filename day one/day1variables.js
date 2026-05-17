9a1ab7ddc3b536294bf4118f28bf1a07cc365e8a
Neshan Niroula <neshanniroula2@gmail.com>
2026-05-17 10:47:35 +0545

    additional code

--- day one/day1variables.js
+++ day one/day1variables.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+console.log("Hello World")
+let a = 10
+const PI = 3.14
+var msg = "Hello World"
+console.log(a, PI, msg)
+
+//function declaration
+function display(){
+    var x = 100
+    console.log("inside display:", x)
+}
+display()
+// console.log("outsite display:", x)
