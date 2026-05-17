b5b4b0f1789295ce565de62c7f3966ccfe4389da
Neshan Niroula <neshanniroula2@gmail.com>
2026-05-17 11:04:47 +0545

    none

--- day one/day1variables.js
+++ day one/day1variables.js
@@ -5,9 +5,32 @@ var msg = "Hello World"
 console.log(a, PI, msg)
 
 //function declaration
-function display(){
+function disp(){
     var x = 100
     console.log("inside display:", x)
 }
-display()
+disp()
 // console.log("outsite display:", x)
+
+//block scope bariables (let and const)
+if(true){
+    var test = "Test"
+    let price = 100
+    console.log("Pric:", price)
+
+}
+console.log(test)
+// console.log(price)
+
+//arrow funciton
+
+let add = (a,b) => a+b
+let res = add(10,51)
+console.log("re:", res)
+
+//display
+let display = (name)=>{
+
+    console.log("hello", name)
+}
+display("Hari")
