59bde699343ea93a6de272c8ddaa049af0e748ba
Neshan Niroula <neshanniroula2@gmail.com>
2026-05-26 08:48:53 +0545

    hello world

---
+++ day2/data.js
@@ -0,0 +1,12 @@
+let PI = 3.14
+let c = "3*10*8"
+let gOnEarth = 9.8
+ //default export
+export default PI
+//named export
+export {c, gOnEarth}
+
+let CalcArea = (r) =>{
+    return 
+}
+export {CalcArea}
---
+++ day2/importexport.js
@@ -0,0 +1,9 @@
+import PI, {c, gOnEarth, CalcArea} from "./data.js"
+
+console.log(PI)
+console.log(c)
+console.log(gOnEarth)
+console.log(CalcArea(5))
+
+let res = CalcArea(5)
+console.log("Area of circle with radius 5 is: ", res)
